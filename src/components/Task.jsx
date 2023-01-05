96ac10071c95f0fbddce3ba4d1e6681b3100b8d0
Maneesh Cheema <ayechico21@gmail.com>
2023-01-06 03:42:15 +0530

    added taskComplete functionality

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,11 +1,16 @@
 import React from "react";
-
+import { useDispatch } from "react-redux";
+import { taskDone } from "../store/tasks";
 const Task = ({ task }) => {
+  const dispatch = useDispatch();
+  const handleOnDone = () => {
+    dispatch(taskDone(task));
+  };
   return (
     <>
       <h3>{task.name}</h3>
       <p>{task.isDone ? "Completed" : "Active"}</p>
-      <button>Done</button>
+      <button onClick={handleOnDone}>Done</button>
       <button>Delete</button>
     </>
   );
--- src/store/tasks.js
+++ src/store/tasks.js
@@ -1,13 +1,7 @@
 import { combineReducers } from "redux";
 
 const ADD_TASK = "ADD_TASK";
-
-export function addTask(task) {
-  return {
-    type: ADD_TASK,
-    task,
-  };
-}
+const TASK_DONE = "TASK_DONE";
 
 const defaultTasks = [
   {
@@ -16,6 +10,22 @@ const defaultTasks = [
   },
 ];
 
+/*************************ACTIONS*************************** */
+export function addTask(task) {
+  return {
+    type: ADD_TASK,
+    task,
+  };
+}
+
+export function taskDone(task) {
+  return {
+    type: TASK_DONE,
+    task,
+  };
+}
+
+/*********************************REDUCER************************ */
 function tasks(state = defaultTasks, action) {
   switch (action.type) {
     case ADD_TASK:
@@ -24,6 +34,17 @@ function tasks(state = defaultTasks, action) {
         {
           name: action.task,
           isDone: false,
+        },
+      ];
+    case TASK_DONE:
+      const task = state.find((t) => action.task.name === t.name); // Get task to be completed
+      const tasks = state.filter((t) => action.task.name !== t.name); // All other tasks
+
+      return [
+        ...tasks,
+        {
+          name: task.name,
+          isDone: true,
         },
       ];
 
